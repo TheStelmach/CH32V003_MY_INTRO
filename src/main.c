51c436be79c40e9eadb693b1e2ebaade44544406
TheStelmach <82737125+TheStelmach@users.noreply.github.com>
2024-06-30 17:12:07 -0400

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,4 +1,13 @@
 #include "main.h"
+#include <stdio.h>
+
+typedef enum
+{
+    false = 0,
+    true = 1
+} bool;
+
+bool servoTurned = false;
 
 int main(void) {
 
@@ -8,9 +17,10 @@ int main(void) {
     RCC->CTLR |= ((uint32_t)0x01) | (((uint32_t)0x01)<<24); // HSI & PLL ON
     RCC->CFGR0 = ((uint32_t)0x00);
     RCC->APB2PCENR = ((uint32_t)0x00);
-    RCC->APB2PCENR |= ((uint32_t)0x01) | (((uint32_t)0x01)<<4) | (((uint32_t)0x01)<<9) 
-                    | (((uint32_t)0x01)<<11) | (((uint32_t)0x01)<<14); // Clock for AFIO | PORTC | ADC1 | TIM1 | USART1
-    RCC->APB1PCENR = ((uint32_t)0x00);
+    RCC->APB2PCENR |= ((uint32_t)0x01) | (((uint32_t)0x01)<<4) | ((uint32_t)0x01<<5) | (((uint32_t)0x01)<<9) 
+                    | (((uint32_t)0x01)<<11) | (((uint32_t)0x01)<<14); // Clock for AFIO | PORTC | PORTD | ADC1 | TIM1 | USART1
+	RCC->APB1PRSTR |= RCC_APB1Periph_TIM2;
+	RCC->APB1PRSTR &= ~RCC_APB1Periph_TIM2;
     RCC->APB1PCENR |= (((uint32_t)0x01)<<28); // Clock for PVD, important to activate before configuring the PWR register
     RCC->APB1PCENR |= ((uint32_t)0x01); // Clock for TIM2
     RCC->RSTSCKR = ((uint32_t)0x01); // Only the first bit of the register is set, LSI enabled
@@ -22,31 +32,39 @@ int main(void) {
     GPIOC->CFGLR |= (((uint32_t)0x03)<<16); // LED
     GPIOC->CFGLR |= (((uint32_t)0x02)<<14); // pushbutton
 
-//------------------------------
+
+    GPIOD->CFGLR &= ~(0xf<<(4*4));
+	GPIOD->CFGLR |= (((uint32_t)0x0B)<<16); // PP_AF @ 30MHz
 
     AFIO->PCFR1 = ((uint32_t)0x00); // Default pin mapping
 
     TIM2->CTLR1 = ((uint16_t)0x00);
-    TIM2->CTLR1 |= ((uint16_t)0x0001) | ((uint16_t)0x0001<<7);
+    TIM2->CTLR1 |= ((uint16_t)0x01) | ((uint16_t)0x01<<7);
     TIM2->CTLR2 = ((uint16_t)0x00);
     TIM2->CTLR2 |= (((uint16_t)0x04)<<4); // Questionable, I don't know how this should be set up yet
     TIM2->SMCFGR = ((uint16_t)0x00);
+    TIM2->SWEVGR |= TIM_UG;
     TIM2->CHCTLR1 = ((uint16_t)0x00);
-    TIM2->CHCTLR1 |= (((uint16_t)0x06)<<4); // Channel 1 output compare mode set (110)
-    TIM2->CHCTLR1 |= (((uint16_t)0x03)<<2); // Channel 1 fast enable bit & preload enable bit
-    TIM2->PSC = ((uint16_t)0x01);
+    TIM2->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1 | TIM_OC1PE;
+    TIM2->CCER |= TIM_CC1E | (TIM_CC1P & 0xff);
+    TIM2->PSC = ((uint16_t)0x00);
     TIM2->ATRLR = (uint32_t)479999; // 20ms
-    TIM2->CH1CVR = 443999u; // 1.5ms high at the end of every 20ms cycle, MAYBE NEED TO REVERSE, 35999u INSTEAD of 443999u
+    TIM2->CH1CVR = (uint32_t)455999; // 1ms high at the end of every 20ms cycle
     
-
-
+    
 
     while(1) 
     {
-        if ((GPIOC->INDR & ((uint32_t)0x08))) GPIOC->BCR = GPIO_Pin_4;
-        else GPIOC->BSHR = GPIO_Pin_4;
+        if ((GPIOC->INDR & ((uint32_t)0x08))) 
+        {
+            GPIOC->BSHR = GPIO_Pin_4;
+            toggle_servo_pos();
+            Delay_Ms(100);
+        }
+        else GPIOC->BCR = GPIO_Pin_4;
 
-        if ((PWR->CSR & ((uint16_t)0x0004)) != 0) warning_blink(); // if PVD0 flag is set - blink the LED
+        if ((PWR->CSR & ((uint16_t)0x0004)) != 0) warning_blink(); // if PVD0 flag is set - blink the LED*/
+
     }
 }
 // 1 tick takes 4.1666666667e-8 seconds
@@ -54,6 +72,16 @@ int main(void) {
 
 void toggle_servo_pos(void)
 {
+    if (servoTurned == false) 
+    {
+        TIM2->CH1CVR = 23999u;
+        servoTurned = true;
+    }
+    else 
+    {
+        TIM2->CH1CVR = 47999u;
+        servoTurned = false;
+    }
     return;
 }
 
